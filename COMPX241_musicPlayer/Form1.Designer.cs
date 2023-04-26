fe14c19fb25e90ca17670e0ec8c346cf647d686c
henryhitchins <hitchinsinc@gmail.com>
2023-04-27 11:00:09 +1200

    Baseline UI, Will improve later

---
+++ COMPX241_musicPlayer/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace COMPX241_musicPlayer
+{
+    partial class musicPlayer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonNext = new System.Windows.Forms.Button();
+            this.buttonPlay = new System.Windows.Forms.Button();
+            this.buttonPause = new System.Windows.Forms.Button();
+            this.buttonPrevious = new System.Windows.Forms.Button();
+            this.menuStripUpload = new System.Windows.Forms.MenuStrip();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // buttonNext
+            // 
+            this.buttonNext.Location = new System.Drawing.Point(489, 390);
+            this.buttonNext.Name = "buttonNext";
+            this.buttonNext.Size = new System.Drawing.Size(75, 23);
+            this.buttonNext.TabIndex = 0;
+            this.buttonNext.Text = "Next";
+            this.buttonNext.UseVisualStyleBackColor = true;
+            // 
+            // buttonPlay
+            // 
+            this.buttonPlay.Location = new System.Drawing.Point(408, 350);
+            this.buttonPlay.Name = "buttonPlay";
+            this.buttonPlay.Size = new System.Drawing.Size(75, 23);
+            this.buttonPlay.TabIndex = 1;
+            this.buttonPlay.Text = "Play";
+            this.buttonPlay.UseVisualStyleBackColor = true;
+            // 
+            // buttonPause
+            // 
+            this.buttonPause.Location = new System.Drawing.Point(489, 350);
+            this.buttonPause.Name = "buttonPause";
+            this.buttonPause.Size = new System.Drawing.Size(75, 23);
+            this.buttonPause.TabIndex = 2;
+            this.buttonPause.Text = "Pause";
+            this.buttonPause.UseVisualStyleBackColor = true;
+            // 
+            // buttonPrevious
+            // 
+            this.buttonPrevious.Location = new System.Drawing.Point(327, 390);
+            this.buttonPrevious.Name = "buttonPrevious";
+            this.buttonPrevious.Size = new System.Drawing.Size(75, 23);
+            this.buttonPrevious.TabIndex = 3;
+            this.buttonPrevious.Text = "Previous";
+            this.buttonPrevious.UseVisualStyleBackColor = true;
+            // 
+            // menuStripUpload
+            // 
+            this.menuStripUpload.Location = new System.Drawing.Point(0, 0);
+            this.menuStripUpload.Name = "menuStripUpload";
+            this.menuStripUpload.Size = new System.Drawing.Size(834, 24);
+            this.menuStripUpload.TabIndex = 4;
+            this.menuStripUpload.Text = "Upload";
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Location = new System.Drawing.Point(327, 350);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(75, 23);
+            this.buttonStop.TabIndex = 6;
+            this.buttonStop.Text = "Stop";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 328);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(810, 16);
+            this.progressBar1.TabIndex = 7;
+            // 
+            // musicPlayer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(834, 450);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.buttonPrevious);
+            this.Controls.Add(this.buttonPause);
+            this.Controls.Add(this.buttonPlay);
+            this.Controls.Add(this.buttonNext);
+            this.Controls.Add(this.menuStripUpload);
+            this.MainMenuStrip = this.menuStripUpload;
+            this.Name = "musicPlayer";
+            this.Text = "musicPlayer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonNext;
+        private System.Windows.Forms.Button buttonPlay;
+        private System.Windows.Forms.Button buttonPause;
+        private System.Windows.Forms.Button buttonPrevious;
+        private System.Windows.Forms.MenuStrip menuStripUpload;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ COMPX241_musicPlayer/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace COMPX241_musicPlayer
+{
+    public partial class musicPlayer : Form
+    {
+        public musicPlayer()
+        {
+            InitializeComponent();
+        }
+    }
+}
